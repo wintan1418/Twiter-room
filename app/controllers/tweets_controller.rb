baa29d58a1551fc06c52cbcf99c5c243286c5319
wintan1418 <wintan1418@gmail.com>
2022-09-22 15:33:21 +0100

    Add first features

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,34 @@
+class TweetsController < ApplicationController
+  
+  before_action :authenticate_user!
+ 
+  def index
+    @tweet = Tweet.new
+    @tweets =Tweet.all.order(created_at: :asc )
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+
+    respond_to do |format|
+      if @tweet.save
+      format.turbo_stream 
+      else
+        format.html do
+          flash[:tweet_errors] = @tweet.erros.full_messages
+          redirect_to root_path
+        end
+    end
+  end
+end
+
+  def destroy
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  # get 'tweets/index'
+  # get 'tweets/create'
+  # get 'tweets/destroy'
+  # get 'tweet/index'
+  # get 'tweet/create'
+  # get 'tweet/destroy'
     authenticate :user, lambda { |u| u.admin? } do
       mount Sidekiq::Web => '/sidekiq'
     end
+    resources :tweets, except: [:edit, :update]
 
+
+    resources :profiles
 
   devise_for :users
-  root to: 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'tweets#index'
+ 
 end
